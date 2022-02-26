b7244edd6ba1f0153f95f31bd78abb27b3d929dd
Pupillen <m15351201779@163.com>
2022-02-26 20:00:00 +0800

    read data from txt

--- test/build/CMakeFiles/Makefile.cmake
+++ test/build/CMakeFiles/Makefile.cmake
@@ -49,6 +49,7 @@ set(CMAKE_MAKEFILE_PRODUCTS
 # Dependency information for all targets:
 set(CMAKE_DEPEND_INFO_FILES
   "CMakeFiles/filtertest.dir/DependInfo.cmake"
+  "CMakeFiles/rmfiltertest.dir/DependInfo.cmake"
   "CMakeFiles/main.dir/DependInfo.cmake"
   "CMakeFiles/filter.dir/DependInfo.cmake"
   )
